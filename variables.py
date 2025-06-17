12fefd17e42ebf1bf43e2782aa00ebd3f8bc8893
crich <christianricciardi@outlook.com>
2025-06-16 23:39:12 -0700

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,14 @@
+# Introduction to Python lessons! Variables!
+
+# declares variables and prints them to the screen in a string
+
+age = 26                                                        # declares a variable called age and assigns it the integer 26
+my_age = age                                                    # declares a variable called my_age and assigns it to the variable age, this will hold whatever value is stored in the variable age
+name = "Christian"                                              # declares a variable called name and assigns it the string "Christian"
+is_male = True                                                  # declares a variables called is_male and assigns a boolean value of True
+
+print("My name is", name, "and I am", age, "years old.")        # prints a string with the variables name and age inserted into it
+print("Am I a male?:", is_male)                                    # prints a question to the screen and returns the variable is_male which holds the boolean value of True
+
+
+
