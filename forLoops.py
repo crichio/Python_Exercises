a8a8a17a20a1831dee9f315c0c058028ad826cb4
crich <christianricciardi@outlook.com>
2025-06-24 12:18:58 -0700

    Add files via upload

---
+++ forLoops.py
@@ -0,0 +1,8 @@
+# an exercise in Python for loops
+
+# this code loops over numbers 1 through 10 and prints them to the screen
+
+for i in range(1, 11):      # uses variable i to loop over ranges 1-11, starting at 1 and stopping at 11
+    print(i)                # prints current value of i variable
+
+# Output: integers 1 through 10
