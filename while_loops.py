a3398548ceb0b7e27c6e7d3266fdce8dbc218e68
crich <christianricciardi@outlook.com>
2025-06-28 21:14:51 -0700

    Add files via upload

---
+++ while_loops.py
@@ -0,0 +1,15 @@
+# while loops
+
+# code asks the user to guess a number between 1 and 10, repeats this until the guess is the correct number
+
+correct_number = 6                                            # declares a varible called correct number and assigns it the integer 6
+guess = None                                                  # declares a varibale called guess and assigns it the value None
+
+while guess != correct_number:                                # while the guess variable is not equal to the number in the correct number varible...
+    guess = int(input("Guess a number between 1 and 10: "))   # prints guess varible to the screen and changes the value of guess variable to a user input which asks the user to guess a number and converts their response into an integer
+
+print("Correct! You guessed the right number.")               # once the guess varible is equal to the correct number varible, print this string to the screen
+
+
+
+# think you can guess the correct number? ;)
